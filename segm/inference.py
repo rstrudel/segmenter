021bd2fce136476e50ead39a3d98b8e34ace40e9
rstrudel <rstrudel@gmail.com>
2021-07-03 13:25:52 +0200

    main: add inference script

---
+++ segm/inference.py
@@ -0,0 +1,62 @@
+import click
+from pathlib import Path
+from PIL import Image
+import numpy as np
+import torchvision.transforms.functional as F
+
+import segm.utils.torch as ptu
+
+from segm.data.utils import STATS
+from segm.data.ade20k import ADE20K_CATS_PATH
+from segm.data.utils import dataset_cat_description, seg_to_rgb
+
+from segm.model.factory import load_model
+from segm.model.utils import inference
+
+
+@click.command()
+@click.option("--model-path", type=str)
+@click.option("--input-dir", "-i", type=str, help="folder with input images")
+@click.option("--output-dir", "-o", type=str, help="folder with output images")
+@click.option("--gpu/--cpu", default=True, is_flag=True)
+def main(model_path, input_dir, output_dir, gpu):
+    ptu.set_gpu_mode(gpu)
+
+    model_dir = Path(model_path).parent
+    model, variant = load_model(model_path)
+    normalization_name = variant["dataset_kwargs"]["normalization"]
+    normalization = STATS[normalization_name]
+
+    cat_names, cat_colors = dataset_cat_description(ADE20K_CATS_PATH)
+
+    input_dir = Path(input_dir)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    for filename in input_dir.iterdir():
+        pil_im = Image.open(filename).copy()
+        im = F.pil_to_tensor(pil_im).float() / 255
+        im = F.normalize(im, normalization["mean"], normalization["std"])
+        im = im.to(ptu.device).unsqueeze(0)
+
+        im_meta = dict(flip=False)
+        logits = inference(
+            model,
+            [im],
+            [im_meta],
+            ori_shape=im.shape[2:4],
+            window_size=variant["inference_kwargs"]["window_size"],
+            window_stride=variant["inference_kwargs"]["window_stride"],
+            batch_size=2,
+        )
+        seg_map = logits.argmax(0)
+        seg_rgb = seg_to_rgb(seg_map.unsqueeze(0), cat_colors)
+        seg_rgb = (255 * seg_rgb.cpu().numpy()).astype(np.uint8)
+        pil_seg = Image.fromarray(seg_rgb[0])
+
+        pil_blend = Image.blend(pil_im, pil_seg, 0.5).convert("RGB")
+        pil_blend.save(output_dir / filename.name)
+
+
+if __name__ == "__main__":
+    main()
